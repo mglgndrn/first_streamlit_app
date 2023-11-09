0f1b550c7a319dd2c0daa2dd09b64dd0b263bc27
mglgndrn <147598647+mglgndrn@users.noreply.github.com>
2023-11-09 17:12:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit
-import pandas
-import requests 
 
 from urllib.error import URLError
 streamlit.title("My Mom's New Healthy Diner")
@@ -10,17 +8,20 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# import pandas
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+
+streamlit.stop()
 #New section to display fruityvice api reponse
 streamlit.header('Fruityvice Fruit Advice!')
 
@@ -29,9 +30,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response.json())
 
 
-streamlit.stop()
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
 
 
 #Create the repeatable code block  (called a function) 
