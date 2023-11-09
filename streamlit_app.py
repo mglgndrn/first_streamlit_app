fb5a12d6aa73afad871341e141f5b82918ad867a
mglgndrn <147598647+mglgndrn@users.noreply.github.com>
2023-11-09 14:42:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,6 @@ try:
 
 # import snowflake.connector
 
-
-streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
