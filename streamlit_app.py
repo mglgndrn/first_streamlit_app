43f700c9e9bd0cdc795847469306a24cc88f82ce
mglgndrn <147598647+mglgndrn@users.noreply.github.com>
2023-11-10 17:28:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ try:
         streamlit.dataframe(back_from_function)
        
 except URLError as e:
-    streamlit.stop()     
+streamlit.stop()  
+    
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
